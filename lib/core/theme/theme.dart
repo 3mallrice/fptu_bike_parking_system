f46a8af7f99ac8cac73fbe4429a8f3ebb9c4af02
Đức Bùi <117875129+3mallrice@users.noreply.github.com>
2024-04-23 00:23:55 +0700

    [3mallrice][config text style]
    Dark mode - light mode

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../const/frondend/color_const.dart';
 
 ThemeData lightMode = ThemeData(
@@ -10,11 +11,50 @@ ThemeData lightMode = ThemeData(
     secondary: ColorLightMode.secondaryOrange,
   ),
   dividerColor: ColorLightMode.primaryText,
+  //Display: H1
+  //headline: H2
+  //title: H3
+  //body: normal
+  //label: list
+
   textTheme: const TextTheme(
+    //display
+    displayMedium: TextStyle(
+      color: ColorLightMode.primaryText,
+      fontSize: 22,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'SFProDisplay',
+    ),
+
+    //headline
+    headlineMedium: TextStyle(
+      color: ColorLightMode.primaryText,
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'SFProDisplay',
+    ),
+
+    //title
+    titleMedium: TextStyle(
+      color: ColorLightMode.primaryText,
+      fontSize: 18,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'SFProDisplay',
+    ),
+
+    //body
+    bodyMedium: TextStyle(
+      color: ColorLightMode.primaryText,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'SFProText',
+    ),
+
+    //label
     labelMedium: TextStyle(
       color: ColorLightMode.primaryText,
       fontSize: 16,
-      fontWeight: FontWeight.w500,
+      fontWeight: FontWeight.w400,
       fontFamily: 'SFProText',
     ),
   ),
@@ -29,4 +69,45 @@ ThemeData darkMode = ThemeData(
     secondary: ColorDarkMode.secondaryGray,
   ),
   dividerColor: ColorDarkMode.text,
+  textTheme: const TextTheme(
+    //display
+    displayMedium: TextStyle(
+      color: ColorDarkMode.text,
+      fontSize: 22,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'SFProDisplay',
+    ),
+
+    //headline
+    headlineMedium: TextStyle(
+      color: ColorDarkMode.text,
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'SFProDisplay',
+    ),
+
+    //title
+    titleMedium: TextStyle(
+      color: ColorDarkMode.text,
+      fontSize: 18,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'SFProDisplay',
+    ),
+
+    //body
+    bodyMedium: TextStyle(
+      color: ColorLightMode.primaryText,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'SFProText',
+    ),
+
+    //label
+    labelMedium: TextStyle(
+      color: ColorDarkMode.text,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'SFProText',
+    ),
+  ),
 );
