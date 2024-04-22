c4f1f11f3e7c2d28bf665bf47ed5d1200a1db031
Đức Bùi <117875129+3mallrice@users.noreply.github.com>
2024-04-22 18:08:57 +0700

    [3mallrice][config]

---
+++ lib/core/const/frondend/color_const.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ColorLightMode {
+  static const Color primariOrange = Color(0xFFF37021);
+  static const Color secondaryOrange = Color(0xFFFFF7EF);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color text = Color(0xFF0E1830);
+  static const Color textBold = Color(0xFFfC7321);
+}
+
+class ColorDarkMode {
+  static const Color grayFacebook = Color(0xFF18191A);
+  static const Color grayFacebookBold = Color(0xFF121212);
+  static const Color grayFacebookLight = Color(0xFF242526);
+  static const Color grayFacebookLighter = Color(0xFF3A3B3C);
+  static const Color grayFacebookLightest = Color(0xFF484A4D);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color text = Color(0xFFF5F5F7);
+}
---
+++ lib/core/helper/asset_helper.dart
@@ -0,0 +1,3 @@
+class AssetHelper {
+  static const String imgLogo = '';
+}
