1852caee2ecc417fef0fef33771816fd05098e59
Đức Bùi <117875129+3mallrice@users.noreply.github.com>
2024-04-24 18:52:17 +0700

    [3mallrice][home screen]

--- lib/core/helper/asset_helper.dart
+++ lib/core/helper/asset_helper.dart
@@ -1,3 +1,9 @@
 class AssetHelper {
   static const String imgLogo = 'assets/imgs/Bai_Logo_NoBackground.png';
+  static const String banner1 = 'assets/imgs/banner1.jpg';
+  static const String banner2 = 'assets/imgs/banner2.jpg';
+  static const String banner3 = 'assets/imgs/banner3.jpg';
+  static const String banner4 = 'assets/imgs/banner4.jpg';
+  static const String banner5 = 'assets/imgs/banner5.jpg';
+  static const String bic = 'assets/imgs/BIC.png';
 }
--- lib/representation/home.dart
+++ lib/representation/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fptu_bike_parking_system/component/app_bar_component.dart';
 import 'package:fptu_bike_parking_system/core/helper/asset_helper.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
 class HomeAppScreen extends StatefulWidget {
   const HomeAppScreen({super.key});
@@ -16,22 +16,160 @@ class _HomeAppScreenState extends State<HomeAppScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Positioned(
-              child: Row(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.1,
-                    child: Image.asset(
-                      AssetHelper.imgLogo,
-                      fit: BoxFit.fill,
-                    ),
+        child: Align(
+          child: Stack(
+            children: [
+              Positioned(
+                child: Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: ImageSlideshow(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.27,
+                    indicatorColor: Theme.of(context).colorScheme.secondary,
+                    indicatorBackgroundColor:
+                        Theme.of(context).colorScheme.primary,
+                    initialPage: 0,
+                    indicatorPadding: 5,
+                    autoPlayInterval: 5000,
+                    isLoop: true,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const FullScreenImageDialog(
+                                imagePath: AssetHelper.banner1,
+                              );
+                            },
+                          );
+                        },
+                        child: const ClipRRect(
+                          child: Image(
+                            width: double.infinity,
+                            height: 139,
+                            image: AssetImage(
+                              AssetHelper.banner1,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const FullScreenImageDialog(
+                                imagePath: AssetHelper.banner2,
+                              );
+                            },
+                          );
+                        },
+                        child: const ClipRRect(
+                          child: Image(
+                            width: double.infinity,
+                            height: 139,
+                            image: AssetImage(
+                              AssetHelper.banner2,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const FullScreenImageDialog(
+                                imagePath: AssetHelper.banner3,
+                              );
+                            },
+                          );
+                        },
+                        child: const ClipRRect(
+                          child: Image(
+                            width: double.infinity,
+                            height: 139,
+                            image: AssetImage(
+                              AssetHelper.banner3,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const FullScreenImageDialog(
+                                imagePath: AssetHelper.banner4,
+                              );
+                            },
+                          );
+                        },
+                        child: const ClipRRect(
+                          child: Image(
+                            width: double.infinity,
+                            height: 139,
+                            image: AssetImage(
+                              AssetHelper.banner4,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const FullScreenImageDialog(
+                                imagePath: AssetHelper.banner5,
+                              );
+                            },
+                          );
+                        },
+                        child: const ClipRRect(
+                          child: Image(
+                            width: double.infinity,
+                            height: 139,
+                            image: AssetImage(
+                              AssetHelper.banner5,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FullScreenImageDialog extends StatelessWidget {
+  final String imagePath;
+
+  const FullScreenImageDialog({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      alignment: Alignment.center,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.cover,
         ),
       ),
     );
