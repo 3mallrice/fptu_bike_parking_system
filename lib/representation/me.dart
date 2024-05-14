6b37fdcf740c509ed748de395e0400e8169c6df3
Đức Bùi <117875129+3mallrice@users.noreply.github.com>
2024-05-14 18:54:35 +0700

    [3mallrice][AccountScreen]

--- lib/representation/me.dart
+++ lib/representation/me.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fptu_bike_parking_system/component/shadow_container.dart';
+import 'package:provider/provider.dart';
 
 class MeScreen extends StatefulWidget {
   const MeScreen({super.key});
@@ -12,9 +16,143 @@ class MeScreen extends StatefulWidget {
 class _MeScreenState extends State<MeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Me Screen'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.primary,
+                    width: 2,
+                  ),
+                  color: Theme.of(context).colorScheme.outline,
+                ),
+                child: Text(
+                  'PB',
+                  style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                        fontSize: 64,
+                        fontWeight: FontWeight.w900,
+                        color: Theme.of(context).colorScheme.background,
+                      ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Phuc Bui',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'phucbhse160538@fpt.edu.vn',
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+              const SizedBox(height: 40),
+              ShadowContainer(
+                width: MediaQuery.of(context).size.width * 0.9,
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () => {},
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.account_circle_outlined,
+                              color: Theme.of(context).colorScheme.outline,
+                              size: 28,
+                            ),
+                            const SizedBox(width: 20),
+                            Text(
+                              'View Profile',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(fontSize: 18),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Theme.of(context).colorScheme.outlineVariant,
+                      thickness: 1,
+                    ),
+                    GestureDetector(
+                      onTap: () => {},
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.info_outlined,
+                              color: Theme.of(context).colorScheme.outline,
+                              size: 28,
+                            ),
+                            const SizedBox(width: 20),
+                            Text(
+                              'About Bai',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(fontSize: 18),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Theme.of(context).colorScheme.outlineVariant,
+                      thickness: 1,
+                    ),
+                    GestureDetector(
+                      onTap: () => {},
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.logout_rounded,
+                              color: Theme.of(context).colorScheme.primary,
+                              size: 28,
+                            ),
+                            const SizedBox(width: 20),
+                            Text(
+                              'Logout',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    fontSize: 18,
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
