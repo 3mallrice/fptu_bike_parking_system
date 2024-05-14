eb989a6ac7d2ba899e877a239ee256ed114ba146
Đức Bùi <117875129+3mallrice@users.noreply.github.com>
2024-05-14 17:35:48 +0700

    [3mallrice][commit]

--- lib/representation/bais_Screen.dart
+++ lib/representation/bais_Screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fptu_bike_parking_system/component/shadow_container.dart';
 
 class BaisScreen extends StatefulWidget {
   const BaisScreen({super.key});
@@ -17,52 +16,50 @@ class _BaisScreenState extends State<BaisScreen> {
       body: SingleChildScrollView(
         child: Align(
           alignment: Alignment.center,
-          child: ShadowContainer(
-            padding: EdgeInsets.zero,
-            width: MediaQuery.of(context).size.width * 0.9,
-            margin: const EdgeInsets.only(top: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Total bais',
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        Text(
-                          '2',
-                          style: Theme.of(context).textTheme.displayMedium,
-                        ),
-                      ],
-                    ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    bottomLeft: Radius.circular(10.0),
                   ),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: GestureDetector(
-                    onTap: () {
-                      //kkk
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      color: Theme.of(context).colorScheme.primary,
-                      child: Icon(
-                        Icons.add_rounded,
-                        color: Theme.of(context).colorScheme.background,
-                      ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Total bikes',
+                      style: Theme.of(context).textTheme.titleMedium,
                     ),
+                    Text(
+                      '2',
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            fontWeight: FontWeight.w500,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  //kkk
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Icon(
+                    Icons.add_rounded,
+                    color: Theme.of(context).colorScheme.background,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
