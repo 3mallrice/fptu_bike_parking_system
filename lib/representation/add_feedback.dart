24cd984c106f73c35ed3d65644f89d3979ce1a3d
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-05-16 02:45:40 +0700

    [maotou][add feedback screen]

--- lib/representation/add_feedback.dart
+++ lib/representation/add_feedback.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:fptu_bike_parking_system/component/app_bar_component.dart';
+import 'package:logger/logger.dart';
+
+import '../component/shadow_button.dart';
+import '../component/shadow_container.dart';
 
 class AddFeedbackScreen extends StatefulWidget {
   const AddFeedbackScreen({super.key});
@@ -12,24 +16,254 @@ class AddFeedbackScreen extends StatefulWidget {
 }
 
 class _AddFeedbackScreenState extends State<AddFeedbackScreen> {
+  String? parkingAreaId;
+  String? parkingAreaName;
+  var log = Logger();
+
+  //init list parking area name
+  List<ParkingArea> parkingAreaList = [
+    ParkingArea(name: 'Central Plaza Parking Lot', id: '1'),
+    ParkingArea(name: 'Green Park Bike Station', id: '2'),
+    ParkingArea(name: 'Tesco Lotus Parking Lot', id: '3'),
+    ParkingArea(name: 'Makro Parking Lot', id: '4'),
+    ParkingArea(name: 'Big C Parking Lot', id: '5'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const AppBarCom(
-          leading: true,
-          appBarText: 'Add Feedback',
-        ),
-        body: SingleChildScrollView(
-          child: Align(
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [],
+      appBar: const AppBarCom(
+        leading: true,
+        appBarText: 'Add Feedback',
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height -
+              MediaQuery.of(context).size.height *
+                  0.1, // Adjust for app bar height
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  padding: const EdgeInsets.only(top: 25),
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Title',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge!
+                                .copyWith(
+                                  color: Theme.of(context).colorScheme.outline,
+                                ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          ShadowContainer(
+                            padding: const EdgeInsets.all(10),
+                            height: MediaQuery.of(context).size.height * 0.065,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                suffixIcon: Icon(
+                                  Icons.edit_rounded,
+                                  color:
+                                      Theme.of(context).colorScheme.onSecondary,
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onSecondary,
+                                  ),
+                                ),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color:
+                                        Theme.of(context).colorScheme.outline,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.03,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Parking Area',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge!
+                                .copyWith(
+                                  color: Theme.of(context).colorScheme.outline,
+                                ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              //TODO: show parking area dialog
+                            },
+                            child: ShadowContainer(
+                              padding: const EdgeInsets.all(10),
+                              height:
+                                  MediaQuery.of(context).size.height * 0.065,
+                              // parking area dropdown
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  suffixIcon: Icon(
+                                    Icons.edit_rounded,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onSecondary,
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSecondary,
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.outline,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.03,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Description',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge!
+                                .copyWith(
+                                  color: Theme.of(context).colorScheme.outline,
+                                ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          ShadowContainer(
+                            padding: const EdgeInsets.all(10),
+                            // height: MediaQuery.of(context).size.height * 0.25,
+                            //text area for description
+                            child: TextField(
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 6,
+                              minLines: 6,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Enter your feedback here',
+                                hintStyle: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSecondary,
+                                    ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 50),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 16),
+                              child: Icon(
+                                Icons.lightbulb_circle,
+                                color: Theme.of(context).colorScheme.outline,
+                                size: 20,
+                              ),
+                            ),
+                            // const SizedBox(width: 10),
+                            Expanded(
+                              child: Text(
+                                'Each of your feedback is our power to improve our service day by day.',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall!
+                                    .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSecondary,
+                                    ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: Text(
+                        'By tapping CONFIRM you agree to submit your feedback.',
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                            ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    child: const ShadowButton(
+                      buttonTitle: 'CONFIRM',
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
+}
+
+class ParkingArea {
+  String name;
+  String id;
+  ParkingArea({required this.name, required this.id});
 }
