d976aad407338523fabac87b16babf8684dd51c5
Đức Bùi <117875129+3mallrice@users.noreply.github.com>
2024-06-29 16:56:06 +0700

    [3mallrice][commit]

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Duc Bui\Desktop\DucbuiProVip123\code\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Duc Bui\Desktop\DucbuiProVip123\Sem 9 - capstone\fptu_bike_parking_system"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
