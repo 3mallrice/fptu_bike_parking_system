56eb0535e7d929e11be58c70004bda76f6e59cd9
Đức Bùi <117875129+3mallrice@users.noreply.github.com>
2024-08-08 19:52:57 +0700

    [3mallrice][create insight screen]

---
+++ lib/representation/insight.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InsightScreen extends StatefulWidget {
+  const InsightScreen({super.key});
+
+  @override
+  State<InsightScreen> createState() => _InsightScreenState();
+}
+
+class _InsightScreenState extends State<InsightScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
